b85b6080d014cfca99651c2b174acf8bf9ef70d6
Mbazu Daniel <mbazudaniel97@gmail.com>
2024-06-01 19:45:50 +0100

    feat: worked on the open and close sidebar implementation

--- src/lib/components/shared/Navbar.svelte
+++ src/lib/components/shared/Navbar.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <nav class="w-full bg-white py-4">
-  <div class="flex justify-between items-center w-[96%] mx-auto relative">
+  <div class="flex justify-between items-center px-4 relative">
     <Avatar.Root>
       <Avatar.Image src="../../images/logo-icon.svg" alt="teamlyf logo" />
       <Avatar.Fallback class="bg-primary text-white text-xl font-bold rounded-xl">T</Avatar.Fallback>
