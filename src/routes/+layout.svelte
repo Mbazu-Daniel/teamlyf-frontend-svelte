a2e8e1aa86c8890a0ea05116dbf8e263a4a6c4f4
Oladize1 <104131886+Oladize1@users.noreply.github.com>
2024-06-02 01:08:48 +0100

    feat: working on the chat

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-  import Navbar from "$lib/components/shared/Navbar.svelte";
+  import DirectMessage from "./chat/DirectMessage.svelte";
+  import NavBar from "../lib/components/shared/Navbar.svelte"
   import "../app.css";
   // import Main from "$lib/components/shared/Main.svelte";
 </script>
@@ -8,7 +9,9 @@
     list-style: none;
     box-sizing: border-box; -->
 <div class="m-0 p-0 outline-0 border-0 appearance-none no-underline box-border">
-  <Navbar />
+  <NavBar />
+  <DirectMessage />
+  
   <!-- <Main /> -->
   <main>
     <slot />
