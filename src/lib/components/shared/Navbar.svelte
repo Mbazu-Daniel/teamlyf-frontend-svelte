83082d99ad9448884728adcfff0f6e82c6b097ed
Ifeani-Ani <ifeanyiani155@gmail.com>
2024-07-20 16:57:16 +0100

    fix: color variable from primary primary-500

--- src/lib/components/shared/Navbar.svelte
+++ src/lib/components/shared/Navbar.svelte
@@ -1,16 +1,18 @@
 <script lang="ts">
-  import * as Avatar from "$lib/components/ui/avatar";
-  import SearchBar from "./SearchBar.svelte";
-  import ProfileArea from "./ProfileArea.svelte";
+	import * as Avatar from '$lib/components/ui/avatar';
+	import SearchBar from './SearchBar.svelte';
+	import ProfileArea from './ProfileArea.svelte';
 </script>
 
 <nav class="fixed w-full max-w-screen-2xl mx-auto z-20 bg-sidebar py-4 shadow-md">
-  <div class="flex justify-between items-center px-4 relative">
-    <Avatar.Root>
-      <Avatar.Image src="../../images/logo-icon.svg" alt="teamlyf logo" />
-      <Avatar.Fallback class="bg-primary text-white text-md font-bold rounded-xl">logo</Avatar.Fallback>
-    </Avatar.Root>
-    <SearchBar />
-    <ProfileArea />
-  </div>
+	<div class="flex justify-between items-center px-4 relative">
+		<Avatar.Root>
+			<Avatar.Image src="../../images/logo-icon.svg" alt="teamlyf logo" />
+			<Avatar.Fallback class="bg-primary-500 text-white text-md font-bold rounded-xl"
+				>logo</Avatar.Fallback
+			>
+		</Avatar.Root>
+		<SearchBar />
+		<ProfileArea />
+	</div>
 </nav>
