83d202c378ffd990907d570531904bd3e44c8075
Ifeani-Ani <ifeanyiani155@gmail.com>
2024-07-20 16:00:50 +0100

    update

--- src/lib/components/projects/ProjectColumn.svelte
+++ src/lib/components/projects/ProjectColumn.svelte
@@ -1,27 +1,11 @@
-<script>
-  import ProjectCard from "./ProjectCard.svelte";
-  export let title;
-  export let count;
-  export let projects;
-  export let bgColor;
+<script lang="ts">
+	import ProjectCard from './ProjectCard.svelte';
+	export let title;
+	export let count;
+	export let projects;
+	export let bgColor;
 </script>
 
-  <div class="space-y-4">
-    {#each projects as project}
-      <ProjectCard
-        title={project.title}
-        tags={project.tags}
-        progress={project.progress}
-        date={project.date}
-        members={project.members}
-        comments={project.comments}
-        attachments={project.attachments}
-        priority={project.priority}
-        thumbnailImage={project.thumbnailImage}
-        {bgColor}
-      />
-    {/each}
-  </div>
 <div class={`min-w-[30%] shrink-0 grow-0 basis-1 ${bgColor} p-4 rounded-t-3xl`}>
 	<div class={`flex justify-between mb-4 p-2 capitalize`}>
 		<h5 class="text-[1.6rem] font-bold text-[#282828]">
@@ -35,5 +19,21 @@
  !   ╰───────────────────────────────────────────────────────╯
 -->
 		<div>action</div>
+	</div>
+	<div class="space-y-4">
+		{#each projects as project}
+			<ProjectCard
+				title={project.title}
+				tags={project.tags}
+				progress={project.progress}
+				date={project.date}
+				members={project.members}
+				comments={project.comments}
+				attachments={project.attachments}
+				priority={project.priority}
+				thumbnailImage={project.thumbnailImage}
+				{bgColor}
+			/>
+		{/each}
 	</div>
 </div>
